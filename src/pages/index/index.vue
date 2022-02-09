371b5b4c9f6f19945c0eb23fdd5b119cfd4b0f42
li <you@example.com>
2022-02-09 13:13:19 +0800

    初始化

---
+++ src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="index">
+    test
+  </view>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue"
+export default defineComponent({
+  name: 'Index',
+  components: {
+  }
+})
+</script>
+
+<style>
+</style>
